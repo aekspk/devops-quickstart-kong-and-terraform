21f789afc69169ec51c7ac1acba42c05e2309854
Aekachai.S <aekachai.s@MacBook-Air-khxng-AekachaiS.local>
2026-04-16 11:10:39 +0700

    remove PAT

--- main.tf
+++ main.tf
@@ -44,5 +44,3 @@ resource "digitalocean_project" "terraform" {
 output "droplet_ip" {
   value = digitalocean_droplet.my-node.ipv4_address
 }
-# PAT = dop_v1_dc458c964f92c8dffc40231ec2b50e9de0711b626d9aba3feabbf3066d7214fb
-# SSH = a5:66:7c:ae:d6:40:e9:58:5a:2e:b3:dd:cd:82:36:62
